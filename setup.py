f52012d79b36b26a372c96596a003478fec52845
3amMcspicy <kc3265@columbia.edu>
2021-03-15 15:51:41 +0800

    added code for setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+Install nycbulidings package
+"""
+
+from setuptools import setup
+
+setup(
+    name="nycbuildings",
+    version="0.0.1",
+    packages =[],
+    entry_points={
+    }
+)
