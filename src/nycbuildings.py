b91d7b7398e75ce03d1b63a5095aff6d88eb8907
3amMcspicy <kc3265@columbia.edu>
2021-03-15 18:00:56 +0800

    included function to select wanted columns

--- src/nycbuildings.py
+++ src/nycbuildings.py
@@ -32,32 +32,56 @@ def coordinate_data():
 """
 This section is to remove columns that are unnecessary for mapping the data,
 to save space and computation power. Tables will be merged with TDS # and 
-any inconsistent data will be removed
+any inconsistent data will be removed. Note that the column names in the dataframe
+is formatted differently than the names in the comments.
 """
 
 #Processing water consumption data
 """
-The columns needed for from this dataset are:
+The columns kept from this dataset are:
 Development Name, Borough, Account Name, Location, TDS #, EDP, Revenue Month,
 Service Start Date, Service End Date, Current Charges, Consumption (HCF)
 """
+
+def water_clean():
+    data = water_consumption_data()
+    data_formatted = data[['development_name', 'borough', 'account_name',
+                            'location', 'tds', 'edp', 'revenue_month',
+                            'service_start_date', 'service_end_date',
+                            'current_charges', 'consumption_hcf']]
+    return data_formatted
 
 
 
 #Processing electricity consumption data
 """
-The columns needed for from this dataset are:
+The columns kept from this dataset are:
 Development Name, Borough, Account Name, Location, TDS #, EDP, Revenue Month,
 Service Start Date, Service End Date, Current Charges, Consumption (KWH)
 """
+def electricity_clean():
+    data = electricity_consumption_data()
+    data_formatted = data[['development_name', 'borough', 'account_name',
+                            'location', 'tds', 'edp', 'revenue_month',
+                            'service_start_date', 'service_end_date',
+                            'current_charges', 'consumption_kwh']]
+    return data_formatted
 
 #Processing coordinates data
 """
-The columns needed for from this dataset are:
+The columns kept from this dataset are:
 DEVELOPMENT, TDS #, BUILDING #, BOROUGH, HOUSE #, STREET, ADDRESS, CITY, LATITUDE,
 LONGTITUDE
 """
+def coordinate_clean():
+    data = coordinate_data()
+    data_formatted = data[['development', 'tds', 'building',
+                            'borough', 'house', 'street','address','city',
+                            'latitude','longitude']]
+    return data_formatted
 
+
+#Merging the data with coordinates. 
 
 
 #Plotting map coordinates
