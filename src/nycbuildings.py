90e215d083f52e89c2a224d52bf373cfe3aeef25
3amMcspicy <kc3265@columbia.edu>
2021-03-15 17:14:38 +0800

    changed method to load dataset with pandas

--- src/nycbuildings.py
+++ src/nycbuildings.py
@@ -3,33 +3,61 @@ import requests
 import pandas as pd
 import numpy as np 
 
-#Loading the Datasets 
+#Task 1: Loading the Datasets 
 """
 Line 6 to 29 is loading the dataset from NYC Open Data.
-Each function returns the dataset in JSON format.
+Each function returns the dataset in pandas dataframe format.
 """ 
 #Fetching water consumption data
 def water_consumption_data():
     URL = "https://data.cityofnewyork.us/resource/66be-66yr.json"
-    response = requests.get(url=URL)
-    return response.json()
+    raw_water = pd.read_json(URL)
+    return raw_water
 
-water_consumption_data()
 
 #Fetching electricity consumption data
 def electricity_consumption_data():
     URL_2 = "https://data.cityofnewyork.us/resource/jr24-e7cr.json"
-    response = requests.get(url=URL_2)
-    return response.json()
+    raw_electricity = pd.read_json(URL_2)
+    return raw_electricity
 
 #Fetching geo-coordinate data
 def coordinate_data():
     URL_3 = "https://data.cityofnewyork.us/resource/3ub5-4ph8.json"
-    response = requests.get(url = URL_3)
-    return response.json()
+    raw_coordinate = pd.read_json(URL_3)
+    return raw_coordinate
 
 
-#Formatting Data - not done yet
+#Task 2: Formatting Data - incomplete
+"""
+This section is to remove columns that are unnecessary for mapping the data,
+to save space and computation power. Tables will be merged with TDS # and 
+any inconsistent data will be removed
+"""
+
+#Processing water consumption data
+"""
+The columns needed for from this dataset are:
+Development Name, Borough, Account Name, Location, TDS #, EDP, Revenue Month,
+Service Start Date, Service End Date, Current Charges, Consumption (HCF)
+"""
+
+
+
+#Processing electricity consumption data
+"""
+The columns needed for from this dataset are:
+Development Name, Borough, Account Name, Location, TDS #, EDP, Revenue Month,
+Service Start Date, Service End Date, Current Charges, Consumption (KWH)
+"""
+
+#Processing coordinates data
+"""
+The columns needed for from this dataset are:
+DEVELOPMENT, TDS #, BUILDING #, BOROUGH, HOUSE #, STREET, ADDRESS, CITY, LATITUDE,
+LONGTITUDE
+"""
+
 
 
 #Plotting map coordinates
