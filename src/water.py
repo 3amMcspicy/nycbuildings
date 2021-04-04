e3673ca5569f4bb873fdb1ff129a1af3101e3611
3amMcspicy <kc3265@columbia.edu>
2021-04-04 17:03:06 +0800

    transferred a function from electricity to here

--- src/water.py
+++ src/water.py
@@ -7,7 +7,7 @@ Fetch data from REST API
 import requests
 import pandas as pd
 
-
+#URL for water dataset
 WATER = "https://data.cityofnewyork.us/resource/66be-66yr.json"
 COLUMNS = [
     # "development_name",
@@ -32,7 +32,7 @@ class Water:
 
     def get_all_data(self):
         """
-        Get all 315K records and return as a dataframe.
+        Get all 34.6K records and return as a dataframe.
         """
         # make request and get response object
         response = requests.get(
@@ -47,7 +47,7 @@ class Water:
         return pd.json_normalize(response.json())[COLUMNS]
 
 
-    def get_data_with_kwargs(self, kwargs):
+    def get_data_with_kwargs(self, **kwargs):
         """
         Get data for only the specified month
         """
@@ -65,11 +65,27 @@ class Water:
 
         # subset to columns of interest
         return data[COLUMNS]
+    
+    def get_data(self, **kwargs):
+        """
+        Get data for only the specified month
+        """
+        # make request and get response object
+        response = requests.get(
+            url=WATER,
+            params={**self.params, **kwargs},
+        )
 
+        # check for errors
+        response.raise_for_status()
+
+        # return as dataframe w/ subset columns
+        data = pd.json_normalize(response.json())
+        return data[COLUMNS]
 
 if __name__ == "__main__":
 
-    # simple test for fetching Electricity data.
+    # simple test for fetching Water data.
     TEST = Water()
     KWARGS = {
         "borough": "MANHATTAN",
