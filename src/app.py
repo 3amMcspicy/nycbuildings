a41f1e85503c92f0514e1de7515bf738f1d47c44
3amMcspicy <kc3265@columbia.edu>
2021-04-14 03:17:30 +0800

    3rd rudimentary map

--- src/app.py
+++ src/app.py
@@ -3,11 +3,19 @@
 """
 A streamlit app
 """
+import pandas as pd
 import streamlit as st
+from PIL import Image
 from streamlit_folium import folium_static
 from data import Data
 from mapping import create_map
 from mapping import create_hex_map
+from mapping import chart_graph
+
+#TODO: tooltips for 2nd map and 3rd
+#TODO: fix radio optioning
+#TODO: speed up timing
+#TODO: 3rd map's consumption_hcf and consumption_kwh
 
 
 #borough selection
@@ -61,9 +69,14 @@ def build_maps(data, year1, year2):
 def hex_map(data):
     create_hex_map(DATA_1)
 
-
-
-
+def display_example():
+    hexagon_image = Image.open('../map/hexagon_layer.jpg')
+    folium_image = Image.open('../map/folium_layer.jpg')
+    col1, col2 = st.beta_columns(2)
+    with col1:
+        st.image(hexagon_image, caption='An example of visualizing consumption over the months')
+    with col2:
+         st.image(folium_image, caption='An example of visualizing mean consumption over the years')
 if __name__ == "__main__":
 
     # draw the webapp header
@@ -71,7 +84,7 @@ if __name__ == "__main__":
 
     # display an example while full data loads
     # ...
-
+    display_example()
 
 
     borough_selection = st.sidebar.selectbox("Please choose a borough", ('MANHATTAN', 'BROOKLYN', 'QUEENS', 'BRONX', 'STATEN ISLAND',))
@@ -91,8 +104,6 @@ if __name__ == "__main__":
         st.success('App is ready')
 
 
-    # draw the data matrix header
-    #build_data_view(DATA.tds_by_year)
 
     # build several maps
     #build_maps(DATA, 2020, 2015)
@@ -101,28 +112,31 @@ if __name__ == "__main__":
     # ...
     time = list(DATA.data.revenue_month.unique())
     time.sort()
-    #time_selection = st.select_slider("Year-Month",time)
+
     
-    #with st.spinner(text='Loading data'):
-        #DATA_1 = DATA.data[DATA.data["revenue_month"] == time_selection]
-        #st.success('App is ready')
     
     toggled_map = st.radio('Please select a map view', ('Mean consumption change between years', 
-                                'Visual consumption over month', 'Consumption by borough'))
+                                'Visual consumption over month', 'Consumption chart'))
     
     if toggled_map == 'Mean consumption change between years':
+        
+        # draw the data matrix header
         build_data_view(DATA.tds_by_year)
-        #need unique years as list of options then offer using st slider
 
-        build_maps(DATA, 2020, 2015)
-        st.write(
-            """
-            The map above shows the change in consumption* between the years selected. 
-            Red circles indicate a decrease, whilst green circles indicate an increase.
+        #Date input by user to compare between years
+        second_date = st.number_input('Please enter the earlier year to compare',min_value=2013, max_value=2020, value =2015)
+        first_date = st.number_input('Please enter the later year to compare',min_value=2014, max_value=2021, value = 2020)
+        if st.button('Go'):
 
-            *consumption is the average consumption for that place in a year
-            """
-            )
+            build_maps(DATA, first_date, second_date)
+            st.write(
+                """
+                The map above shows the change in consumption* between the years selected. 
+                Red circles indicate a decrease, whilst green circles indicate an increase.
+
+                *consumption is the average consumption for that place in a year
+                """
+                )
     
     elif toggled_map == 'Visual consumption over month':
         time_selection = st.select_slider("Year-Month",time)
@@ -133,6 +147,11 @@ if __name__ == "__main__":
         #time_selection = st.select_slider("Year-Month",time)
         hex_map(DATA_1)
     
+    elif toggled_map == 'Consumption chart':
+        #chart_data = pd.DataFrame(DATA.tds_sum_year)
+        #chart_data.reset_index(inplace=True)
+        chart_graph(DATA)
+        #st.write(chart_data)
     
     
 
